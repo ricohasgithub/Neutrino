b6e39346a6e077406d1a4ac9aa4427b4ef24db39
ricozhuthegreat <ricozhuthegreat@hotmail.com>
2019-12-17 17:31:18 -0500

    Initial Commit
     - Added Constants cpp and Header files

---
+++ Neutrino/Constants.h
@@ -0,0 +1,21 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+namespace Neutrino {
+
+    class Constants
+    {
+        public:
+            Constants();
+            virtual ~Constants();
+
+            static const double GRAVITY;
+
+        protected:
+
+        private:
+    };
+
+}
+
+#endif // CONSTANTS_H
---
+++ Neutrino/src/Constants.cpp
@@ -0,0 +1,18 @@
+#include "Constants.h"
+
+namespace Neutrino
+{
+
+    static const double GRAVITY = -9.8;
+
+    Constants::Constants()
+    {
+        //ctor
+    }
+
+    Constants::~Constants()
+    {
+        //dtor
+    }
+
+}
