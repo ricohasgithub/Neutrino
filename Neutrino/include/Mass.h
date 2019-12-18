8e9960da39bfe9f5c1b1ee84d6a16c6d00459c49
ricozhuthegreat <ricozhuthegreat@hotmail.com>
2019-12-18 09:34:54 -0500

    Updated Mass Methods
     - Added new frunctions for getting the average/center x and y values so that it could be used for center of mass calculations

--- Neutrino/include/Mass.h
+++ Neutrino/include/Mass.h
@@ -1,28 +1,79 @@
 #ifndef MASS_H
 #define MASS_H
 
+#include <vector>
+
+namespace Neutrino
+{
+
+struct Point3D
+{
+    double x;
+    double y;
+    double z;
+};
 
 class Mass
 {
-    public:
 
-        Mass(double m);
-        virtual ~Mass();
+public:
 
-        double getMass () {
-            return mass;
+    Mass(double m, std::vector<double> nx, std::vector<double> ny, std::vector<double> nz);
+    virtual ~Mass();
+
+    double getMass ()
+    {
+        return mass;
+    }
+
+    double updateMass (double nMass)
+    {
+        mass = nMass;
+    }
+
+    std::vector<Point3D> getFrame ()
+    {
+        return frame;
+    }
+
+    // Get the average x value. Used in center of mass calculations
+    double getCenterX()
+    {
+
+        double sum = 0.0;
+
+        for (int p=0; p<frame.size(); p++)
+        {
+            sum += frame[p].x;
         }
 
-        double updateMass (double nMass) {
-            mass = nMass;
+        return (sum / frame.size());
+
+    }
+
+    // Get the average y value. Used in center of mass calculations
+    double getCenterY()
+    {
+
+        double sum = 0.0;
+
+        for (int p=0; p<frame.size(); p++)
+        {
+            sum += frame[p].y;
         }
 
-    protected:
+        return (sum / frame.size());
 
-    private:
+    }
 
-        double mass;
+protected:
+    double mass;
+    std::vector<Point3D> frame;
+
+private:
 
 };
+
+}
 
 #endif // MASS_H
--- Neutrino/src/Mass.cpp
+++ Neutrino/src/Mass.cpp
@@ -1,8 +1,18 @@
 #include "Mass.h"
 
-Mass::Mass(double m)
+using namespace Neutrino;
+
+Mass::Mass(double m, std::vector<double> nx, std::vector<double> ny, std::vector<double> nz)
 {
     mass = m;
+    // Assume size of vectors x, y and z are of equal length
+    for (int p=0; p<nx.size(); p++) {
+        Point3D temp;
+        temp.x = nx[p];
+        temp.y = ny[p];
+        temp.z = nz[p];
+        frame.push_back(temp);
+    }
 }
 
 Mass::~Mass()
